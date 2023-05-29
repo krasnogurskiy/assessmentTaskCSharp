803e731ccb2295a0b623dd95de20171cbdbc014f
krasnogurskiy <andrey.kranogurskiy@gmail.com>
2023-05-30 00:02:18 +0300

    upd

---
+++ Class.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    public class Kolo
+    {
+        public Bitmap obrazek;
+        public Bitmap tempObrazek;
+        public float kat;
+        public int[] wartosciStanu;
+        public int stan;
+
+        public Kolo()
+        {
+            tempObrazek = new Bitmap(Properties.Resources.kolo);
+            obrazek = new Bitmap(Properties.Resources.kolo);
+            wartosciStanu = new int[] { 425, 225, 375, -1, 25, 275, 400, 325, 100, 0, 200, 50, 350, 3000, 175, 475, 300, 125, 75, 500 };
+            kat = 0.0f;
+        }
+
+    }
+}
+
+
+
+//namespace app
+//{
+//   // Клас гравця
+//    public class Player
+//    {
+//        public string Name { get; set; }
+//        public int Score { get; set; }
+//    }
+
+//    // Клас колеса Фортуни
+//    public class GameWheel
+//    {
+//        public void Spin()
+//        {
+//            // Логіка обертання колеса
+//        }
+
+//        public int GetPoints()
+//        {
+//            // Повертає кількість очків, що випали після обертання колеса
+//            return 0;
+//        }
+//    }
+
+//    // Клас питання
+//    public class Question
+//    {
+//        public string QuestionText { get; set; }
+//        public string Answer { get; set; }
+//    }
+
+//    // Клас гри
+//    public class Game
+//    {
+//        private int currentPlayerIndex;
+//        private int currentRound;
+//        private GameWheel gameWheel;
+
+//        public List<Player> Players { get; set; }
+//        public List<Question> Questions { get; set; }
+
+//        public Game()
+//        {
+//            Players = new List<Player>();
+//            Questions = new List<Question>();
+//            gameWheel = new GameWheel();
+//        }
+
+//        public void StartGame()
+//        {
+//            currentRound = 1;
+//            currentPlayerIndex = 0;
+
+//            // Ініціалізуємо стан гри та відображаємо графічний інтерфейс
+//            InitializeGame();
+//            UI.DisplayGameBoard();
+
+//            // Цикл раундів гри
+//            while (currentRound <= 4)
+//            {
+//                UI.DisplayRound(currentRound);
+
+//                // Отримуємо активного гравця
+//                Player currentPlayer = Players[currentPlayerIndex];
+
+//                // Гравець обертає колесо Фортуни
+//                gameWheel.Spin();
+//                int points = gameWheel.GetPoints();
+
+//                UI.DisplaySpinResult(points);
+
+//                // Гравець вибирає дію (відкриття літери або спробу вгадати повністю)
+//                PlayerAction playerAction = UI.GetPlayerAction();
+
+//                if (playerAction == PlayerAction.GuessFullWord)
+//                {
+//                    string guessedWord = UI.GetPlayerGuess();
+//                    HandlePlayerGuess(currentPlayer, guessedWord, points);
+//                }
+//                else if (playerAction == PlayerAction.OpenLetter)
+//                {
+//                    //char letter = UI.GetLetterFromPlayer();
+//                    HandleOpenLetter(currentPlayer, letter, points);
+//                }
+
+//                // Перехід до наступного гравця
+//                currentPlayerIndex = (currentPlayerIndex + 1) % Players.Count;
+
+//                // Якщо всі гравці виконали свій хід, переходимо до наступного раунду
+//                if (currentPlayerIndex == 0)
+//                    currentRound++;
+//            }
+
+//            // Гра завершена, відображаємо фінальні результати
+//           // UI.DisplayFinalScores(Players);
+//        }
+
+//        private void InitializeGame()
+//        {
+//            // Ініціалізуємо список питань та гравців
+//            // ...
+
+//            // Заповнюємо список питань (Questions) з відповідями
+//            // ...
+//        }
+
+//        private void HandlePlayerGuess(Player player, string guessedWord, int points)
+//        {
+//            // Перевіряємо, чи відповідає вгадане слово правильній відповіді
+//            // ...
+
+//            // Оновлюємо бали гравця та відображаємо інформацію на інтерфейсі
+//            // ...
+
+//            // Перехід до наступного гравця
+//            NextPlayerTurn();
+//        }
+
+//        private void HandleOpenLetter(Player player, char letter, int points)
+//        {
+//            // Перевіряємо, чи присутня відкрита літера у відповіді
+//            // ...
+
+//            // Оновлюємо бали гравця та відображаємо інформацію на інтерфейсі
+//            // ...
+
+//            // Перехід до наступного гравця
+//            NextPlayerTurn();
+//        }
+
+//        private void NextPlayerTurn()
+//        {
+//            // Перехід до наступного гравця
+//            currentPlayerIndex = (currentPlayerIndex + 1) % Players.Count;
+
+//            // Відображення інформації про наступного гравця на інтерфейсі
+//            //UI.DisplayNextPlayer(Players[currentPlayerIndex]);
+//        }
+//    }
+
+
+//    // Клас графічного інтерфейсу
+//    public class UI
+//    {
+//        public void DisplayMainMenu()
+//        {
+//            // Відображення головного меню гри
+//        }
+
+//        public void DisplayGameBoard()
+//        {
+//            // Відображення ігрової дошки
+//        }
+
+//        public void DisplayQuestion(Question question)
+//        {
+//            // Відображення питання
+//        }
+
+//        public void DisplayPlayerScores(List<Player> players)
+//        {
+//            // Відображення балів гравців
+//        }
+
+//    }
+
+//}
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,190 @@ namespace app
 {
     public partial class Form1 : Form
     {
+        Kolo koloFortuny;
+        bool wheelIsMoved;
+        float startWheelTimes;
+        float wheelTimes;
+        //Timer gameTimer;
+        Timer wheelTimer;
+        Random rand;
+        //int los;
+
         public Form1()
         {
+            rand = new Random();
+            //los = rand.Next(0, hasla.Length);
+            koloFortuny = new Kolo();
             InitializeComponent();
+            wheelIsMoved = false;
+            wheelTimer = new Timer();
+            wheelTimer.Interval = 10;
+            wheelTimer.Tick += WheelTimer_Tick;
+            wheelTimes = 100;
+        }
+
+
+        public Bitmap rotateImage()
+        {
+            Bitmap rotatedImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(rotatedImage))
+            {
+                g.TranslateTransform((pictureBox1.Width / 2), pictureBox1.Height / 2); //set the rotation point as the center into the matrix
+                g.RotateTransform(koloFortuny.kat); //rotate
+                g.TranslateTransform(-pictureBox1.Width / 2, -pictureBox1.Height / 2); //restore rotation point into the matrix
+                g.DrawImage(koloFortuny.tempObrazek, new Point(0, 0)); //draw the image on the new bitmap
+            }
+            return rotatedImage;
+        }
+
+        public static Bitmap RotateImage(Image image, float angle)
+        {
+            return RotateImage(image, new PointF((float)image.Width / 2, (float)image.Height / 2), angle);
+        }
+
+        public static Bitmap RotateImage(Image image, PointF offset, float angle)
+        {
+            if (image == null)
+                throw new ArgumentNullException("image");
+
+            //create a new empty bitmap to hold rotated image
+            Bitmap rotatedBmp = new Bitmap(image.Width, image.Height);
+            rotatedBmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            //make a graphics object from the empty bitmap
+            Graphics g = Graphics.FromImage(rotatedBmp);
+
+            //Put the rotation point in the center of the image
+            g.TranslateTransform(offset.X, offset.Y);
+
+            //rotate the image
+            g.RotateTransform(angle);
+
+            //move the image back
+            g.TranslateTransform(-offset.X, -offset.Y);
+
+            //draw passed in image onto graphics object
+            g.DrawImage(image, new PointF(0, 0));
+
+            return rotatedBmp;
+        }
+
+        private void RotateImage(PictureBox pb, Image img, float angle)
+        {
+            if (img == null || pb.Image == null)
+                return;
+
+            Image oldImage = pb.Image;
+            pb.Image = RotateImage(img, angle);
+            if (oldImage != null)
+            {
+                oldImage.Dispose();
+            }
+        }
+
+        private void WheelTimer_Tick(object sender, EventArgs e)
+        {
+            //label4.Visible = false;
+
+            if (wheelIsMoved && wheelTimes > 0)
+            {
+                koloFortuny.kat += wheelTimes / 10;
+                koloFortuny.kat = koloFortuny.kat % 360;
+                RotateImage(pictureBox1, koloFortuny.obrazek, koloFortuny.kat);
+                wheelTimes--;
+            }
+
+            koloFortuny.stan = Convert.ToInt32(Math.Ceiling(koloFortuny.kat / 18));
+
+            if (koloFortuny.stan == 0)
+            {
+                koloFortuny.stan = 0;
+            }
+            else
+            {
+                koloFortuny.stan -= 1;
+            }
+
+            //label1.Text = Convert.ToString(koloFortuny.kat);
+            //label2.Text = Convert.ToString(koloFortuny.stan);
+            labelScoreInt.Text = Convert.ToString(koloFortuny.wartosciStanu[koloFortuny.stan]);
+
+            //gra.stawka = koloFortuny.wartosciStanu[koloFortuny.stan];
+            //gra.podpowiedz[2] = "Grasz o " + gra.stawka;
+
+            if (wheelTimes == 0)
+            {
+                wheelIsMoved = false;
+                wheelTimer.Stop();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            wheelIsMoved = true;
+            Random rand = new Random();
+            wheelTimes = rand.Next(150, 200);
+            startWheelTimes = wheelTimes;
+
+                
+            wheelTimer.Start();
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
