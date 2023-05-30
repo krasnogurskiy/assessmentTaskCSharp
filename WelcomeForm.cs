3651703941ff3dd6baab0e980854a7b9ddacf7d4
krasnogurskiy <andrey.kranogurskiy@gmail.com>
2023-05-30 16:25:40 +0300

    upd

--- WelcomeForm.cs
+++ WelcomeForm.cs
@@ -16,5 +16,15 @@ namespace app
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void player2_cb_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
